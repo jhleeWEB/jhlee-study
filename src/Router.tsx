6bcdba7a8eae66a1b49c0d928f5ccfd793c99e17
jhlee <jhlee89898989@gmail.com>
2025-02-06 23:11:40 +0900

    fix typo fixed
    hompage -> homepage

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import Home from '@pages/Home';
 
 const Routers = () => {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Routes>
 				<Route path='/' element={<Home />} />
 			</Routes>
