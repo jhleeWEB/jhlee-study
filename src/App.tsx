3ed14a79c85dac11b76093de5d2ed50a1eff0e7e
jhlee <jhlee89898989@gmail.com>
2025-02-06 21:40:23 +0900

    setup route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import TestingComponent from '@components/TestingComponent';
+import Routers from './Router';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-				<TestingComponent />
-			</header>
-		</div>
+		<React.StrictMode>
+			<Routers />
+		</React.StrictMode>
 	);
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import App from './App';
+import TestingComponent from '@components/TestingComponent';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+root.render(<App />);
