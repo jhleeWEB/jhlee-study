ea46af4494557b83f8f3e58611f358f3fb90cfbe
jhlee <jhlee89898989@gmail.com>
2025-02-06 21:54:56 +0900

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router';
 import App from './App';
-import TestingComponent from '@components/TestingComponent';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
