e65983f224d82603423b17d5e464c97397cc647e
jhlee <jhlee89898989@gmail.com>
2025-02-17 22:46:56 +0900

    feat react-query added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import Home from './pages/Home';
 import Routers from './Routers';
 import Layout from './pages/Layout';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 function App() {
+	const queryClient = new QueryClient();
+
 	return (
 		<React.StrictMode>
-			<RecoilRoot>
-				<BrowserRouter>
-					<Layout>
-						<Routers />
-					</Layout>
-				</BrowserRouter>
-			</RecoilRoot>
+			<QueryClientProvider client={queryClient}>
+				<RecoilRoot>
+					<BrowserRouter>
+						<Layout>
+							<Routers />
+						</Layout>
+					</BrowserRouter>
+				</RecoilRoot>
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
 		</React.StrictMode>
 	);
 }
